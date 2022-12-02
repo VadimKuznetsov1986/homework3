58f2b9eb2f13a320ece3c8198a1ad8b882e47841
Kuznetsov Vadim <kuzibundius@gmail.com>
2022-12-03 00:58:33 +0700

    finish

--- task1.py
+++ task1.py
@@ -11,6 +11,6 @@ lst = fill_int_digit_list(int(input('Введите длину списка: '))
 print(f'Исходный список: {lst}')
 
 total = 0
-for i in range(1, len(lst) - 1, 2):
+for i in range(1, len(lst), 2):
     total += lst[i]
 print(f'Сумма элементов стоящих на нечетной позиции = {total}')
